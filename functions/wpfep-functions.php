9282d165f03449c621310b2dbd2b35ab89c07a74
Sébastien SERRE <sebastien@thivinfo.com>
2020-11-06 08:04:10 +0100

    Correct Checkbox value
    The options set in `wpfep_fields_$tab_id` filter are not took in consideration. Now they are.

--- functions/wpfep-functions.php
+++ functions/wpfep-functions.php
@@ -305,10 +305,20 @@ function wpfep_field($field, $classes, $tab_id, $user_id)
 
                 /* if the type is set to a checkbox */
                 case 'checkbox':
+	                $options = wpfep_field_get_options($field);
                     ?>
 					<input type="hidden" name="<?php echo esc_attr($tab_id); ?>[<?php echo esc_attr($field['id']); ?>]" id="<?php echo esc_attr($field['id']); ?>" value="0" <?php checked($current_field_value, '0'); ?> />
-					<input type="checkbox" name="<?php echo esc_attr($tab_id); ?>[<?php echo esc_attr($field['id']); ?>]" id="<?php echo esc_attr($field['id']); ?>" value="1" <?php checked($current_field_value, '1'); ?> />
+                <?php
+                    /* loop through each option */
+	                foreach ($options as $option) {
+	                    ?>
+
+                    <input type="checkbox" name="<?php echo esc_attr($tab_id); ?>[<?php echo esc_attr($field['id']);
+                    ?>]" id="<?php echo esc_attr($field['id']); ?>" value="<?php echo $option['value']?>" <?php checked
+                    ($current_field_value, $option['value']); ?> />
 					<?php
+                        echo $option['name'];
+                    }
 
                     /* break out of the switch statement */
                     break;
