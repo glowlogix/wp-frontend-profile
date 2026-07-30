9464680a84a61d242140a23a581817a87987c75f
Faizan <faizan.ali@glowlogix.com>
2026-07-30 16:12:33 +0500

    url issue and exit function issue fixed

--- inc/class-wpfep-login.php
+++ inc/class-wpfep-login.php
@@ -55,7 +55,7 @@ if (! class_exists('WPFEP_Login')) {
             add_filter('login_url', array( $this, 'filter_login_url' ), 10, 2);
             add_filter('logout_url', array( $this, 'filter_logout_url' ), 10, 2);
             add_filter('lostpassword_url', array( $this, 'filter_lostpassword_url' ), 10, 2);
-            add_filter('authenticate', array( $this, 'successfully_authenticate' ), 30, 3);
+            add_filter('authenticate', array( $this, 'successfully_authenticate' ), 30, 1);
         }
 
         /**
@@ -149,6 +149,10 @@ if (! class_exists('WPFEP_Login')) {
          */
         public function filter_login_url($url, $redirect)
         {
+            if (! $this->get_login_url()) {
+                return $url;
+            }
+
             return $this->get_action_url('login', $redirect);
         }
 
@@ -162,6 +166,10 @@ if (! class_exists('WPFEP_Login')) {
          */
         public function filter_logout_url($url, $redirect)
         {
+            if (! $this->get_login_url()) {
+                return $url;
+            }
+
             return $this->get_action_url('logout', $redirect);
         }
 
@@ -175,6 +183,10 @@ if (! class_exists('WPFEP_Login')) {
          */
         public function filter_lostpassword_url($url, $redirect)
         {
+            if (! $this->get_login_url()) {
+                return $url;
+            }
+
             return $this->get_action_url('lostpassword', $redirect);
         }
 
@@ -378,7 +390,7 @@ if (! class_exists('WPFEP_Login')) {
                 } else {
                     $redirect = $this->login_redirect();
                     wp_redirect(apply_filters('wpfep_login_redirect', $redirect, $user));
-                    exit;
+                    return;
                 }
             }
         }
@@ -423,7 +435,7 @@ if (! class_exists('WPFEP_Login')) {
 
                 $redirect_to = ! empty($_REQUEST['redirect_to']) ? sanitize_text_field(wp_unslash($_REQUEST['redirect_to'])) : add_query_arg(array( 'loggedout' => 'true' ), $this->get_login_url());
                 wp_safe_redirect($redirect_to);
-                exit();
+                return;
             }
         }
 
@@ -444,7 +456,7 @@ if (! class_exists('WPFEP_Login')) {
                     if ($this->retrieve_password()) {
                         $url = add_query_arg(array('checkemail' => 'confirm'), $this->get_login_url());
                         wp_redirect($url);
-                        exit;
+                        return;
                     }
                 } else {
                     wp_die();
@@ -519,7 +531,7 @@ if (! class_exists('WPFEP_Login')) {
                             do_action('wpfep_customer_reset_password', $user);
 
                             wp_redirect(add_query_arg('reset', 'true', remove_query_arg(array('key', 'login'))));
-                            exit;
+                            return;
                         }
                     }
                 }
@@ -662,13 +674,11 @@ if (! class_exists('WPFEP_Login')) {
         /**
          * Successful authenticate when enable email verification in registration.
          *
-         * @param object $user     return user.
-         * @param string $username return username.
-         * @param string $password return password.
+         * @param object $user return user.
          *
          * @return object
          */
-        public function successfully_authenticate($user, $username, $password)
+        public function successfully_authenticate($user)
         {
             if (! is_wp_error($user)) {
                 if ($user->ID) {
@@ -839,17 +849,17 @@ if (! class_exists('WPFEP_Login')) {
                 $register_page_id = wpfep_get_option('register_page', 'wpfep_pages');
                 if ($register_page_id) {
                     wp_safe_redirect(get_permalink($register_page_id));
-                    exit;
+                    return;
                 }
             }
 
             if (in_array($action, array('lostpassword', 'resetpass', 'rp', 'logout'), true)) {
                 wp_safe_redirect(add_query_arg('action', $action, $login_url));
-                exit;
+                return;
             }
 
             wp_safe_redirect($login_url);
-            exit;
+            return;
         }
 
         /**
